c81009140ca4aedaad1309d23a264835f2738e6f
SBAndrew28 <andyblount28@yahoo.com>
2019-02-24 23:03:45 -0500

    Categorized ~5000 SPEs with updated sorting technique

--- p1/p1.py
+++ p1/p1.py
@@ -11,6 +11,7 @@ from read_waveform import read_waveform as rw
 from write_waveform import write_waveform
 import time
 import os
+from p1_sort import p1_sort
 
 ################################
 # Signal Parameters
@@ -39,70 +40,11 @@ lowpass = signal.firwin(numtaps, cutoff = wc/np.pi, window = 'blackman')    # bl
 # plt.grid(True)
 # plt.show()
 
-# fil = 'C:/watchman/data/20181212_watchman_spe/C2--waveforms--00020.txt'
-# (t,v,hdr) = rw(fil,nhdr)
-# y1 = signal.lfilter(lowpass, 1.0, v)
-# y2 = signal.filtfilt(lowpass,1.0, v)
-# z1 = y1[numtaps:len(y1)-1]
-# z2 = y2[numtaps:len(y1)-1]
-# ty = t[numtaps:len(y1)-1]
-# plt.figure()
-# plt.plot(t,v,'b')
-# plt.plot(ty,z2,'r',linewidth=3)
-# # plt.plot(ty,z1,'c')
-# plt.grid(True)
-# plt.show()
-
-j = 0
-# k = 0
-# h = 0
-j = 7000
-for i in range(Nloops):
+j = 22500
+for i in range(j, Nloops):
     # print i
-
-    fname = 'C:/watchman/data/20181212_watchman_spe/C2--waveforms--%05d.txt' % j
-    spe_wname = 'C:/watchman/signal_chain_studies/d1/data/D1--waveforms--%05d.txt' % j
-    spe_not_there = 'C:/watchman/signal_chain_studies/d1/not_spe/D1--not_spe--%05d.txt' % j
-    spe_unsure = 'C:/watchman/signal_chain_studies/d1/unsure_if_spe/D1--unsure--%05d.txt' % j
-    if os.path.isfile(spe_wname):
-        pass
-    elif os.path.isfile(spe_not_there):
-        pass
-    elif os.path.isfile(spe_unsure):
-        pass
-    else:
-        fil = open(fname)
-        (t,v,hdr) = rw(fname,nhdr)
-
-        y = signal.filtfilt(lowpass,1.0, v)
-        y2 = y[numtaps:len(y)-1]
-        t2 = t[numtaps:len(y)-1]
-
-
-        # print(f)
-        if min(y2[0:len(y2)-1-numtaps]) < -0.012:
-
-            plt.figure()
-            plt.plot(t,v,'k')
-            plt.plot(t2,y2,'m',linewidth=2.5)
-            plt.grid(True)
-            print('Displaying file #%05d' % j)
-            plt.show(block = False)
-            plt.pause(1.5)
-            plt.close()
-            #
-            spe_check = 'pre-loop initialization'
-            while spe_check != 'y' and spe_check != 'n' and spe_check != 'u':
-                spe_check = raw_input('Is there a single visible SPE? "y" or "n"\n')
-            if spe_check == 'y':
-                # Write data file to processed SPE folder
-                write_waveform(t2, y2, spe_wname, hdr)
-            elif spe_check == 'n':
-                write_waveform(t2, y2, spe_not_there, hdr)
-            elif spe_check == 'u':
-                write_waveform(t2, y2, spe_unsure, hdr)
-            print('file #%05d: Done' % j)
-    j += 1
+    p1_sort(i)
+    # j += 1
 
 
 
---
+++ p1/p1_sort.py
@@ -0,0 +1,91 @@
+# Creating the sorting function for p1
+# Requires the import of read_waveform and write_waveform to work properly
+
+import sys
+sys.path.append('../analysis/')
+import numpy as np
+import matplotlib.pyplot as plt
+from read_waveform import read_waveform as rw
+from write_waveform import write_waveform
+from scipy import signal
+import os
+
+
+
+
+def p1_sort(fnum):
+    nhdr = 5
+    fsps = 20000000000.   # Hz, Samples per second
+    fc = 250000000.       # Hz, Filter cutoff frequency
+    wc = 2.*np.pi*fc/fsps # Discrete radial frequency
+    numtaps = 51              # filter order + 1, chosen for balance of good performance and small transient size
+    lowpass = signal.firwin(numtaps, cutoff = wc/np.pi, window = 'blackman')    # blackman windowed lowpass filter
+
+    fname = 'C:/watchman/data/20181212_watchman_spe/C2--waveforms--%05d.txt' % fnum
+    spe_wname = 'C:/watchman/signal_chain_studies/d1/data/D1--waveforms--%05d.txt' % fnum
+    spe_not_there = 'C:/watchman/signal_chain_studies/d1/not_spe/D1--not_spe--%05d.txt' % fnum
+    spe_unsure = 'C:/watchman/signal_chain_studies/d1/unsure_if_spe/D1--unsure--%05d.txt' % fnum
+    if os.path.isfile(spe_wname):
+        pass
+    elif os.path.isfile(spe_not_there):
+        pass
+    elif os.path.isfile(spe_unsure):
+        pass
+    else:
+        fil = open(fname)
+        (t,v,hdr) = rw(fname,nhdr)
+
+        y = signal.filtfilt(lowpass,1.0, v)
+        y2 = y[numtaps:len(y)-1]
+        t2 = t[numtaps:len(y)-1]
+
+        y_flip = -1 * y2
+        peaks, _ = signal.find_peaks(y_flip, 0.0115, distance = 350)
+        y_peaks = y2[peaks]
+        t_peaks = t2[peaks]
+
+        # print(f)
+        if len(peaks) > 1: #min(y2[0:len(y2)-1-numtaps]) < -0.0125:
+            pass
+            plt.figure()
+            plt.plot(t,v,'b')
+            plt.plot(t2,y2,'r',linewidth=2.5)
+            plt.plot(t_peaks, y_peaks,'x',color='yellow')
+            # plt.plot(t,-0.012*np.ones(len(v)),'k--')
+            plt.grid(True)
+            print('Displaying file #%05d' % fnum)
+            plt.show(block = False)
+            plt.pause(1.5)
+            plt.close()
+            #
+            spe_check = 'pre-loop initialization'
+            while spe_check != 'y' and spe_check != 'n' and spe_check != 'u':
+                spe_check = raw_input('Is there a single visible SPE? "y" or "n"\n')
+            if spe_check == 'y':
+                # Write data file to processed SPE folder
+                write_waveform(t2, y2, spe_wname, hdr)
+            elif spe_check == 'n':
+                write_waveform(t2, y2, spe_not_there, hdr)
+            elif spe_check == 'u':
+                write_waveform(t2, y2, spe_unsure, hdr)
+            print('file #%05d: Done' % fnum)
+            # print('mean is %f' % np.mean(y2))
+        elif len(peaks) == 1:
+            # plt.figure()
+            # plt.plot(t,v,'b')
+            # plt.plot(t2,y2,'r',linewidth=2.5)
+            # plt.plot(t_peaks, y_peaks,'x',color='yellow')
+            # plt.grid(True)
+            # print('Displaying file #%05d' % fnum)
+            # plt.show()
+            write_waveform(t2, y2, spe_wname, hdr)
+    return
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(prog="p1 sort", description="Sorting through raw data to find good SPEs")
+    parser.add_argument("--fnum", type = int, help = 'file number to begin at')
+    args = parser.parse_args()
+
+    p1_sort(args.fnum)
