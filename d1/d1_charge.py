5625e150bb043482e48e904b5320d202d079d354
SBAndrew28 <andyblount28@yahoo.com>
2019-03-15 11:21:53 -0400

    making further changes to the charge module

--- d1/d1_charge.py
+++ d1/d1_charge.py
@@ -17,26 +17,39 @@ for i in range(j,Nloops):
     y = y / min(y[start_index:end_index])
     t_window = t[start_index:end_index]
     y_window = y[start_index:end_index]
-    norm_thresh = 0.05
-    norm_thresh2 = 0.1
+    norm_thresh = 0.5
 
     max_location = y == 1
     max_index = [i for i, x in enumerate(max_location) if x]
     max_index = int(max_index[0])
 
+    index_b4_max = range(start_index, max_index)
     index_af_max = range(max_index, end_index)
+
+    y_b4_max = y[index_b4_max]
     y_af_max = y[index_af_max]
+    t_b4_max = t[index_b4_max]
     t_af_max = t[index_af_max]
 
-    check10_fall = y_af_max <= 0.1
-    t_10fall = t_af_max[check10_fall]
-    y_10fall = y_af_max[check10_fall]
-    t_10 = t_10fall[0]
-    y_10 = y_10fall[0]
+    check50_rise = y_b4_max >= norm_thresh
+    check50_fall = y_af_max <= norm_thresh
+    t_50fall = t_af_max[check50_fall]
+    y_50fall = y_af_max[check50_fall]
+    t_50rise = t_b4_max[check50_rise]
+    y_50rise = y_b4_max[check50_rise]
 
-    # plt.plot(t_window,y_window,'b')
-    # plt.plot([t_window[0],t_window[-1]],[norm_thresh, norm_thresh],'k--')
-    # plt.plot([t_window[0],t_window[-1]],[norm_thresh2, norm_thresh2],'b--')
-    # plt.plot(t_10, y_10, 'ro')
-    # plt.grid(True)
-    # plt.show()
+    t_50r = t_50rise[0]
+    y_50r = y_50rise[0]
+    t_50f = t_50fall[0]
+    y_50f = y_50fall[0]
+
+    fwhm = t_50f - t_50r
+    t_samp = t[1] - t[0]
+    fwhm_samp = int(fwhm / t_samp)
+    print('Full width half max samples: %i' % fwhm_samp)
+
+    plt.plot(t_window,y_window,'b-')
+    plt.plot([t_window[0],t_window[-1]],[norm_thresh, norm_thresh],'k--')
+    plt.plot([t_50r, t_50f], [y_50r, y_50f], 'ro')
+    plt.grid(True)
+    plt.show()
